fd1bd517a8f68445d252d0cecf641e37cf8d474b
sime1997 <sime.gaurina@gmail.com>
2021-10-21 20:39:41 +0200

    dodano za testove

--- test/mtest.cpp
+++ test/mtest.cpp
@@ -9,5 +9,5 @@ TEST(Konstrukcija, zbrajanje){
     mat2(0,0) = 1; mat2(0,1) = 1; mat2(1,0) = 0; mat2(1,1) = 1;
 
     MatrixXd mat3 = mat1+mat2;
-    ASSERT_DOUBLE_EQ(mat3(0,0), mat1(0,0)+mat2(0,0));
+    ASSERT_DOUBLE_EQ(mat3(0,1), mat1(0,0)+mat2(0,0));
 }
